e09313bf06dbbaf19f6b270913f8f30b2ba60eb7
shladdergoo <shladdergoo@users.noreply.github.com>
2021-07-09 14:32:19 +0100

    add handler to send button

--- app/src/main/java/com/datagility/floto/MainActivity.kt
+++ app/src/main/java/com/datagility/floto/MainActivity.kt
@@ -2,10 +2,21 @@ package com.datagility.floto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSend = findViewById<Button>(R.id.btn_send)
+        val txtNote = findViewById<EditText>(R.id.txt_note)
+
+        btnSend.setOnClickListener {
+            Toast.makeText(this@MainActivity, txtNote.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
