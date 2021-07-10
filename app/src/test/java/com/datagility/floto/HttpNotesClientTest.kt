8959105d2774b1525d4edaf008fc4d35b4de492a
shladdergoo <shladdergoo@users.noreply.github.com>
2021-07-10 12:17:14 +0100

    preparing to write tests for HttpNotesClient

---
+++ app/src/test/java/com/datagility/floto/HttpNotesClientTest.kt
@@ -0,0 +1,13 @@
+package com.datagility.floto
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class HttpNotesClientTest {
+
+    @Test
+    fun fooTest() {
+
+     assertEquals(true, false)
+    }
+}
